774a2a98a9b10dc9d684e91402f587b49c22821a
andrea mazzarella <andrea.mazzarella89@gmail.com>
2021-04-22 20:30:08 +0100

    removes a few packages

--- install_cask_packages.sh
+++ install_cask_packages.sh
@@ -1,22 +1,15 @@
 packages_to_install=(
   "adoptopenjdk11"
   "1password"
-  "dbeaver-community"
   "docker"
   "firefox"
-  "keybase"
   "iterm2"
   "intellij-idea"
-  "intellij-idea-ce"
   "google-chrome"
   "postman"
-  "pycharm-ce"
   "slack"
-  "skype"
-  "spectacle"
   "spotify"
   "typora"
-  "tunnelbear"
   "whatsapp"
 )
 
